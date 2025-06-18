0cad893fd780dfb846508bcf1d4a1977911e60a2
fippolo <filippo.fanesi@gmail.com>
2025-06-18 14:27:08 +0200

    first commit

---
+++ FanesiVergariMVC/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using FanesiVergariMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FanesiVergariMVC.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult TestAction()
+        {
+            // ....
+            TestTestActionViewModel vm = new TestTestActionViewModel();
+            vm.nome = "riccardo";
+            vm.cognome = "Cognini";
+            vm.elencoLavori = new string[2] { "professore", "programmatore" };
+
+            return View(vm);
+        }
+    }
+}
---
+++ FanesiVergariMVC/ViewModels/TestTestActionViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FanesiVergariMVC.ViewModels
+{
+    public class TestTestActionViewModel
+    {
+        public string nome {get; set;}
+        public string cognome {get; set;}
+        public string[] elencoLavori {get; set;}
+        
+
+
+    }
+}
