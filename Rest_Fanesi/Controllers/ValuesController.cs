26194e2395ba29c2c318c03605ab73067353263a
fippolo <filippo.fanesi@gmail.com>
2025-06-27 10:09:01 +0200

    aggiunto restapi all'esame

---
+++ Rest_Fanesi/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using FanesiVergari.Modelli;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Rest_Fanesi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public Task<Autovelox[]> Get()
+        {
+            return ServiziAutoveloxItalia.ElencoAutoveloxItalia();
+        }
+
+        [HttpPost]
+        public Task<Autovelox[]> GetPerRegione([FromBody] string regione)
+        {
+            return ServiziAutoveloxItalia.ElencoAutoveloxPerRegione(regione);
+        }
+
+        [HttpGet]
+        public Task<string[]> GetRegioni()
+        {
+            return ServiziAutoveloxItalia.ElencoRegioniDisponibili();
+        }
+    }
+}
